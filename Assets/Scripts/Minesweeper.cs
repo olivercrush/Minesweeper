4ce13a972283e56372dcd85802c1449d136864a4
olivercrush <olivier.beguinti@gmail.com>
2020-11-26 02:32:30 -0500

    Added simple visualization

--- Assets/Scripts/Minesweeper.cs
+++ Assets/Scripts/Minesweeper.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class Minesweeper : MonoBehaviour
 {
+    public GameObject _coveredCellPrefab;
+    public GameObject _coveredBombPrefab;
+
     private bool[,] _grid;
+    private List<GameObject> _objectGrid;
 
     // Start is called before the first frame update
     void Start()
     {
-        GenerateGrid();
+        _grid = GenerateGrid();
+        InstantiateObjects(_grid);
+    }
+
+    private void InstantiateObjects(bool[,] grid)
+    {
+        _objectGrid = new List<GameObject>();
+        for (int i = 0; i < grid.GetLength(0); i++) {
+            for (int j = 0; j < grid.GetLength(1); j++) {
+                if (grid[i, j]) {
+                    _objectGrid.Add(Instantiate(_coveredBombPrefab, new Vector3(j, i, 0), Quaternion.identity, transform));
+                }
+                else {
+                    _objectGrid.Add(Instantiate(_coveredCellPrefab, new Vector3(j, i, 0), Quaternion.identity, transform));
+                }
+            }
+        }
     }
 
     private bool[,] GenerateGrid() {
