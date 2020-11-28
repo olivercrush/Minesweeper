4aec9485cf9df07aa49e0701497c199c9696a789
olivercrush <olivier.beguinti@gmail.com>
2020-11-28 00:35:21 -0500

    Added temporary custom cursor

---
+++ Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+    public Texture2D _cursorTexture;
+
+    void Start()
+    {
+        Cursor.SetCursor(_cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+}
